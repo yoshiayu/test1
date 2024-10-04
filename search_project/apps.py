9a34fdd2995baddc8694390f83d6650e84466443
yoshiayu <yoshiayu19771106@gmail.com>
2024-10-04 14:00:51 +0900

    1stcommit

---
+++ search_project/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SearchProjectConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "search_project"
---
+++ search_project/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Product
+
+class SearchForm(forms.Form):
+    query = forms.CharField(
+        label='検索キーワード',
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': '検索したいキーワードを入力'})   
+    )
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'category'] 
+        labels = {
+            'name': '商品名',
+            'description': '説明',
+            'price': '価格',
+            'category': 'カテゴリ',
+        }
